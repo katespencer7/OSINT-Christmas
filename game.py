bf6a5d036eee5178553c7e4034ff6d1c95c6c81d
Kate Spencer <kathleennhspencer@gmail.com>
2026-01-09 10:48:53 -0800

    Working on adding the interactive elements

--- game.py
+++ game.py
@@ -1,31 +1,31 @@
 import pygame
 import pygame.freetype
-from pygame.sprite import Sprite
+from pygame.sprite import Sprite, RenderUpdates
 from pygame.rect import Rect
-import sys
+import sys, json, os
 from enum import Enum
-
-pygame.init()
-
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("BEGIN GAME")
-
-clock = pygame.time.Clock()
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+WIDTH, HEIGHT = 800, 600
 
-pygame.mixer.music.load('assets/sounds/soulomon-b-the-yume-collective-midnight-miracles-436039.mp3')
-pygame.mixer.music.play(-1) # play indefinetly
-click_sound = pygame.mixer.Sound("assets/sounds/90s-game-ui-6-185099.wav")
 
 def create_surface_with_text(text, font_size, text_rgb, bg_rgb=None):
-    # Resize font if needed
+    """ Returns surface with text written on """
     font = pygame.font.Font('assets/ByteBounce.ttf', int(font_size))
     surface = font.render(text, True, text_rgb, bg_rgb)  # antialias=True
     return surface.convert_alpha()
+
+def load_game():
+    ''' For loading game information from save_data.json. '''
+    try:
+        with open("save_data.json", "r") as f:
+            data = json.load(f)
+        return Player(**data)
+    except FileNotFoundError:
+        return Player()  # fresh save
+
 
 class UIElement(Sprite):
     """ An user interface element that can be added to a surface """
@@ -72,14 +72,14 @@ class UIElement(Sprite):
     def rect(self):
         return self.rects[1] if self.mouse_over else self.rects[0]
 
-    def update(self, mouse_pos, mouse_up):
+    def update(self, mouse_pos, mouse_up, sound):
         """ Updates the element's appearance depending on the mouse position
             and returns the button's action if clicked.
         """
         if self.rect.collidepoint(mouse_pos):
             self.mouse_over = True
             if mouse_up:
-                click_sound.play()
+                sound.play()
                 return self.action
         else:
             self.mouse_over = False
@@ -88,13 +88,46 @@ class UIElement(Sprite):
         """ Draws element onto a surface """
         surface.blit(self.image, self.rect)
 
+
 class GameState(Enum):
     QUIT = -1
     TITLE = 0
     NEWGAME = 1
     CHARACTER = 2
+    NEXT_LEVEL = 3
+    
+    PORTLAND = 10
+    CORVALLIS = 11
+    EUGENE = 12
 
-def title_screen(screen):
+
+class Player:
+    """ Stores information about a player """
+
+    def __init__(self, score=0, current_level=1):
+        self.score = score
+        self.current_level = current_level
+
+    def save_game(player):
+        data = {
+            "score": player.score,
+            "current_level": player.current_level,
+        }
+
+        with open("save_data.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+
+class Character: # TODO
+    """ Character selection functions """
+    def __init__(self):
+        return
+
+
+def title_screen(screen, sound=None):
+    background_image = pygame.image.load(os.path.join('assets/background_images/background_pixel.png')).convert()
+    background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))     # Scale the image to fit the window size
+
     begin_element = UIElement(
         center_position=(300, 475),
         font_size=30,
@@ -122,27 +155,59 @@ def title_screen(screen):
         action=GameState.CHARACTER,
     )
 
-    buttons = [begin_element, character_element, quit_element]
+    buttons = RenderUpdates(begin_element, character_element, quit_element)
+    return game_loop(screen, buttons, sound, background_image)
 
-    while True:
-        mouse_up = False
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: # for window closing
-                return GameState.QUIT
-            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                mouse_up = True
-        screen.fill(BLACK)
+def portland_screen(screen, sound=None):
+    background_image = pygame.image.load(os.path.join('assets/background_images/portland_pixel.png')).convert()
+    background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))     # Scale the image to fit the window size
 
-        for button in buttons:
-            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
-            if ui_action is not None:
-                return ui_action
-            button.draw(screen)
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text="<--- Return to menu",
+        action=GameState.NEWGAME,
+    )
 
-        pygame.display.flip()
+    buttons = RenderUpdates(return_btn)
+    return game_loop(screen, buttons, sound, background_image, draw_box=True)
 
+def eugene_screen(screen, sound=None):
+    background_image = pygame.image.load(os.path.join('assets/background_images/eugene_pixel.png')).convert()
+    background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))     # Scale the image to fit the window size
 
-def play_level(screen):
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text="<--- Return to menu",
+        action=GameState.NEWGAME,
+    )
+
+    buttons = RenderUpdates(return_btn)
+    return game_loop(screen, buttons, sound, background_image)
+
+def corvallis_screen(screen, sound=None):
+    background_image = pygame.image.load(os.path.join('assets/background_images/corvallis_pixel.png')).convert()
+    background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))     # Scale the image to fit the window size
+
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text="<--- Return to menu",
+        action=GameState.NEWGAME,
+    )
+
+    buttons = RenderUpdates(return_btn)
+    return game_loop(screen, buttons, sound, background_image)
+
+def character_screen(screen, player, sound=None):
+    ''' Character selection page. Uses Character class for character values '''
     return_btn = UIElement(
         center_position=(140, 570),
         font_size=20,
@@ -152,6 +217,58 @@ def play_level(screen):
         action=GameState.TITLE,
     )
 
+    buttons = RenderUpdates(return_btn)
+    return game_loop(screen, buttons, sound)
+
+def play_level(screen, player, sound=None):
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text="<--- Return to start",
+        action=GameState.TITLE,
+    )
+    nextlevel_btn = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text=f"Next level ({player.current_level + 1})",
+        action=GameState.NEXT_LEVEL,
+    )
+    portland_btn = UIElement(
+        center_position=(200, 100),
+        font_size=30,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text=f"Portland",
+        action=GameState.PORTLAND,
+    )
+    eugene_btn = UIElement(
+        center_position=(400, 100),
+        font_size=30,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text=f"Eugene",
+        action=GameState.EUGENE,
+    )
+    corvallis_btn = UIElement(
+        center_position=(600, 100),
+        font_size=30,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text=f"Corvallis",
+        action=GameState.CORVALLIS,
+    )
+
+    buttons = RenderUpdates(return_btn, nextlevel_btn, portland_btn, eugene_btn, corvallis_btn)
+    return game_loop(screen, buttons, sound)
+
+def game_loop(screen, buttons, sound=None, background=None, draw_box=False):
+    """ Handles game loop until an action is return by a button in the
+    buttons sprite renderer.
+    """
     while True:
         mouse_up = False
         for event in pygame.event.get():
@@ -159,11 +276,33 @@ def play_level(screen):
                 return GameState.QUIT
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 mouse_up = True
-        screen.fill(BLACK)
+        
+        if background:
+            screen.blit(background, (0, 0))
+        else:
+            screen.fill(BLACK)
 
-        ui_action = return_btn.update(pygame.mouse.get_pos(), mouse_up)
-        if ui_action is not None:
-            return ui_action
-        return_btn.draw(screen)
+        if draw_box:
+            level_1 = pygame.Rect(50, 50, 100, 100)
+            pygame.draw.rect(screen, BLACK, level_1)
 
+            level_2 = pygame.Rect(200, 50, 100, 100)
+            pygame.draw.rect(screen, BLACK, level_2)
+
+            level_3 = pygame.Rect(350, 50, 100, 100)
+            pygame.draw.rect(screen, BLACK, level_3)
+
+            level_4 = pygame.Rect(500, 50, 100, 100)
+            pygame.draw.rect(screen, BLACK, level_4)
+
+            level_5 = pygame.Rect(650, 50, 100, 100)
+            pygame.draw.rect(screen, BLACK, level_5)
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up, sound)
+            if ui_action is not None:
+                return ui_action
+
+        buttons.draw(screen)
         pygame.display.flip()
+
--- main.py
+++ main.py
@@ -2,25 +2,46 @@ from game import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("BEGIN GAME")
+    clock = pygame.time.Clock()
     game_state = GameState.TITLE
-    running = True
+        
+    pygame.mixer.music.load('assets/sounds/soulomon-b-the-yume-collective-midnight-miracles-436039.mp3')
+    pygame.mixer.music.play(-1) # play indefinetly
+    click_sound = pygame.mixer.Sound("assets/sounds/90s-game-ui-6-185099.wav")
 
+    running = True
     while running:
         for event in pygame.event.get(): # for window closing
             if event.type == pygame.QUIT:
                 running = False
 
         if game_state == GameState.TITLE:
-            game_state = title_screen(screen)
+            game_state = title_screen(screen, click_sound)
 
-        elif game_state == GameState.NEWGAME:
-            game_state = play_level(screen)
+        if game_state == GameState.NEWGAME:
+            player = load_game()
+            game_state = play_level(screen, player, click_sound)
+
+        if game_state == GameState.NEXT_LEVEL:
+            player.current_level += 1
+            game_state = play_level(screen, player, click_sound)
 
         elif game_state == GameState.CHARACTER:
-            game_state = play_level(screen)
+            game_state = character_screen(screen)
+
+        elif game_state == GameState.PORTLAND: # Portland game screen
+            game_state = portland_screen(screen, click_sound)
+
+        elif game_state == GameState.EUGENE: # Eugene game screen
+            game_state = eugene_screen(screen, click_sound)
+        
+        elif game_state == GameState.CORVALLIS: # Corvallis game screen
+            game_state = corvallis_screen(screen, click_sound)
 
         elif game_state == GameState.QUIT:
+            player.save_game() # save the current game
             running = False
 
         pygame.display.flip()
